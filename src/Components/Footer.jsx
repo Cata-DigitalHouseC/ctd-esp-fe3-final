1445f96b3ebff37d913fb16fb1e49f33f3e6e615
Catalina Hernandez <cataxto@gmail.com>
2023-04-07 20:59:41 -0500

    Organizando el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <p>Powered by</p>
-        <img src="/images/DH.png" alt='DH-logo' />
+          <p>Powered by</p>
+          <img src="/images/DH.png" alt='DH-logo' />
+          <a target="_blank" href="https://www.facebook.com/DigitalHouseColombia/?brand_redir=865681700186683&locale=es_LA"><img className="imgFooter" src="/images/icons8-facebook-nuevo-50.png"  alt="Facebook"/> </a>
+          <a target="_blank" href="https://www.instagram.com/_digitalhouse/?hl=es"><img className="imgFooter" src="/images/icons8-instagram-viejo-50.png"  alt="Instagram"/> </a>
+          <a target="_blank" href="https://www.tiktok.com/@_digitalhouse"><img className="imgFooter" src="/images/icons8-tik-tok-50.png"  alt="Tiktok"/> </a>
+          <a target="_blank" href="https://api.whatsapp.com/send/?phone=573113067719&text&app_absent=0"><img className="imgFooter" src="/images/icons8-whatsapp-50.png"  alt="WhatsApp"/> </a>
     </footer>
   )
 }
